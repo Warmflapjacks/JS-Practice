bedca6c86778eac75b3be2713e1e3fa7169076f6
Micheal LeBlanc <mikeleblanc8489@gmail.com>
2022-12-31 20:45:22 -0500

    Add a multiplication function

--- scripts.js
+++ scripts.js
@@ -2,4 +2,9 @@ let add7 = (number) => {
     return console.log(number + 7);
 }
 
+let multiply = (firstNumber, secondNumber) => {
+    return console.log(firstNumber * secondNumber);
+}
+
 add7(13);
+multiply(2, 5);
