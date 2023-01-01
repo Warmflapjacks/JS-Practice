d964f48452002ba28fde81985968c39cfa80e2f3
Micheal LeBlanc <mikeleblanc8489@gmail.com>
2022-12-31 21:04:10 -0500

    Add function to capitalize first letter of a string

--- scripts.js
+++ scripts.js
@@ -6,5 +6,15 @@ let multiply = (firstNumber, secondNumber) => {
     return console.log(firstNumber * secondNumber);
 }
 
+let capitalize = (aString) => {
+    let newString = aString.toLowerCase();
+    console.log(newString);
+    let firstLetter = newString[0].toUpperCase();
+    let finalString = firstLetter.concat("",newString.slice(1));
+    return console.log(finalString);
+}
+
+
 add7(13);
 multiply(2, 5);
+capitalize("hAPPy NEW year!!!!!!");
